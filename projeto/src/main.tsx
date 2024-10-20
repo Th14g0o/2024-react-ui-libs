98ea7a7db487acda571db212bbd157e1bab8c8db
Th14g0o <geekland135@gmail.com>
2024-10-20 20:18:01 -0300

    Trocando rotas

--- projeto/src/main.tsx
+++ projeto/src/main.tsx
@@ -3,23 +3,17 @@ import { createRoot } from 'react-dom/client';
 import Login from './paginas/Login.tsx';
 import Lista from './paginas/Lista.tsx';
 import { ChakraProvider } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-
-const Rotas = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "lista",
-    element: <Lista />,
-  },
-]);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider>
-      <RouterProvider router={Rotas} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="lista" element={<Lista />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>,
 )
