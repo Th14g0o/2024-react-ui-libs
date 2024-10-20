62d0af7cefce00eb921138de33f51b6cb9fb9098
Th14g0o <geekland135@gmail.com>
2024-10-20 20:06:00 -0300

    Interfaces finalizadas

--- projeto/src/paginas/Lista.tsx
+++ projeto/src/paginas/Lista.tsx
@@ -1,15 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useNavigate, } from "react-router-dom";
 import {
-    Box, Heading, Text, IconButton, FormLabel, FormControl, Input, Stack, ButtonGroup, Button, Textarea,
+    Box, Heading, Text, IconButton, FormLabel, FormControl, Input, Stack, ButtonGroup, Button, Textarea, Checkbox,
     Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel,
     Menu, MenuButton, MenuList, MenuItem,
     CircularProgress, CircularProgressLabel,
     Popover, PopoverTrigger, FocusLock, PopoverContent, useDisclosure,
-    AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter
+    AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter,
+    useToast
 
 } from "@chakra-ui/react";
-import { ArrowBackIcon, HamburgerIcon, DeleteIcon, EditIcon, AddIcon, CheckIcon, CheckCircleIcon, SpinnerIcon } from '@chakra-ui/icons'
+import { ArrowBackIcon, HamburgerIcon, DeleteIcon, EditIcon, AddIcon } from '@chakra-ui/icons'
 
 interface Tarefa {
     idTarefa: number;
@@ -21,7 +22,7 @@ interface Tarefa {
 }
 
 interface TarefaAcordeaoProps extends Tarefa {
-    atualizarTarefa: (id: number, titulo: string, descricao: string) => void;
+    atualizarTarefa: (id: number, titulo: string, descricao: string, ehFinalizado : boolean) => void;
     excluirTarefa: (idTarefa: number) => void;
 }
 
@@ -33,6 +34,7 @@ const TarefaAcordeao: React.FC<TarefaAcordeaoProps> = ({ titulo, descricao, ehFi
 
     const [tituloAtual, setNovoTitulo] = useState(titulo);
     const [descricaoAtual, setNovaDescricao] = useState(descricao);
+    const [ehFinalizadoAtual, setEhFinalizadoAtual] = useState(ehFinalizado);
 
     const escutaModificacaoTexto = (evento: any) => {
         setNovoTitulo(evento.target.value);
@@ -40,9 +42,12 @@ const TarefaAcordeao: React.FC<TarefaAcordeaoProps> = ({ titulo, descricao, ehFi
     const escutaModificacaoDesc = (evento: any) => {
         setNovaDescricao(evento.target.value);
     }
+    const escutaModificacaoStatus = (evento: any) => {
+        setEhFinalizadoAtual(evento.target.checked ? true : false);
+    }
 
     const salvarEdicao = () => {
-        atualizarTarefa(idTarefa, tituloAtual, descricaoAtual);
+        atualizarTarefa(idTarefa, tituloAtual, descricaoAtual, ehFinalizadoAtual);
         onPopoverClose();
     };
 
@@ -91,12 +96,18 @@ const TarefaAcordeao: React.FC<TarefaAcordeaoProps> = ({ titulo, descricao, ehFi
                             <FocusLock persistentFocus={false}>
                                 <Stack spacing={4}>
                                     <FormControl>
-                                        <FormLabel htmlFor={idTarefa.toString()} color="black">Titulo</FormLabel>
+                                        <FormLabel htmlFor={idTarefa.toString()} color="black">Titulo:</FormLabel>
                                         <Input value={tituloAtual} onChange={escutaModificacaoTexto} type="text" ref={firstFieldRef} placeholder='Escreva o titulo aqui...' id={idTarefa.toString()} color="black" />
                                     </FormControl>
                                     <FormControl>
-                                        <FormLabel htmlFor={idTarefa.toString()} color="black">Descrição</FormLabel>
+                                        <FormLabel htmlFor={idTarefa.toString()} color="black">Descrição:</FormLabel>
                                         <Textarea  value={descricaoAtual} onChange={escutaModificacaoDesc} id={idTarefa.toString()} color="black" placeholder='Descreva aqui...' size='sm' />
+                                    </FormControl>
+                                    <FormControl>
+                                        <FormLabel htmlFor={idTarefa.toString()} color="black">Status:</FormLabel>
+                                        <Checkbox size='lg' colorScheme='blue' checked={ehFinalizadoAtual} onChange={escutaModificacaoStatus}>
+                                            <Text color="black">Concluido</Text>
+                                        </Checkbox>
                                     </FormControl>
                                     <ButtonGroup display='flex' justifyContent='flex-end'>
                                         <Button variant='outline' onClick={onPopoverClose}>
@@ -136,16 +147,66 @@ const Lista = () => {
         navegador("/");
     }
 
-    const atualizarTarefa = (idTarefa: number, novoTitulo: string, novaDescricao: string) => {
+    const atualizarTarefa = (idTarefa: number, novoTitulo: string, novaDescricao: string, novoEhFinalizado : boolean) => {
         setTarefas(tarefas.map(tarefa =>
             tarefa.idTarefa == idTarefa
-                ? { ...tarefa, titulo: novoTitulo, descricao: novaDescricao,  dtAlterado: new Date().toLocaleString(), }
+                ? { ...tarefa, titulo: novoTitulo, descricao: novaDescricao, ehFinalizado: novoEhFinalizado,  dtAlterado: new Date().toLocaleString(), }
                 : tarefa
         ));
     };
     const excluirTarefa = (idTarefa: number) => {
         setTarefas(tarefas.filter(tarefa => tarefa.idTarefa !== idTarefa));
     };
+
+    const aviso = useToast()
+
+    const criarTarefa =() =>{
+        const elementoTitulo = document.getElementById('tituloCricao') as HTMLInputElement;
+        const elementoDesc = document.getElementById('descCricao') as HTMLTextAreaElement;
+        if (elementoTitulo && elementoTitulo.value){
+            const maxId = tarefas.length > 0 ? Math.max(...tarefas.map(tarefa => tarefa.idTarefa)) + 1 : 1;
+            if(elementoTitulo.value){
+                setTarefas(
+                    [
+                        ... tarefas,
+                        {   
+                            idTarefa: maxId,
+                            titulo: elementoTitulo.value, 
+                            ehFinalizado: false, 
+                            descricao: elementoDesc && elementoDesc.value? elementoDesc.value : "", 
+                            dtCriado: new Date().toLocaleString(), 
+                            dtAlterado: new Date().toLocaleString() 
+                        }
+                    ]
+                );
+                elementoTitulo.value = "";
+                elementoDesc.value = "";
+            }
+        }
+        else{
+            aviso({
+                position: 'top',
+                title: "Um tarefa precisa ter titulo",
+                status: "warning",
+                isClosable: true,
+              });
+        }
+    }
+
+    const [qtdTarefas, atualizarQtdTarefas] = useState(0);
+    const [qtdTarefasConcluidas, atualizarQtdTarefasConcluidas] = useState(0);
+    const [porcentagemConcluida, setPorcentagemConcluida] = useState(0);
+    useEffect(() => {
+        let qTarefas = tarefas.length;
+        let concluidas = tarefas.filter((tarefa: Tarefa) => tarefa.ehFinalizado).length;
+        if(concluidas != qtdTarefasConcluidas || qTarefas != qtdTarefas){
+            atualizarQtdTarefas(qTarefas);
+            atualizarQtdTarefasConcluidas(concluidas);
+            const totalTarefas = tarefas.length;
+            const novaPorcentagem = totalTarefas > 0 ? Math.trunc((concluidas / totalTarefas) * 100) : 0;
+            setPorcentagemConcluida(novaPorcentagem);
+        }
+    }, );
 
     return (
         <Box minH="100vh" display="flex" alignItems="center" justifyContent="center" bg="gray.100" >
@@ -175,25 +236,24 @@ const Lista = () => {
                 </Heading>
                 <Text color="blue.200" as="h2" size="lg" width="100%" textAlign="center" mb={10}>Bem vindo Usuario</Text>
                 <Box display="flex" mb={10} alignItems="center" justifyContent="center">
-                    <CircularProgress size='120px' value={40} color='blue.200'>
-                        <CircularProgressLabel>40%</CircularProgressLabel>
+                    <CircularProgress size='120px' value={porcentagemConcluida} color='blue.200'>
+                        <CircularProgressLabel>{porcentagemConcluida}%</CircularProgressLabel>
                     </CircularProgress>
                 </Box>
-                <ButtonGroup spacing='3' mb={3}>
-                    <Button leftIcon={<AddIcon />} variant='solid' colorScheme="green" >
-                        Criar Nova
+                <Box display="flex" mb={1} alignItems="center" justifyContent="center" >
+                    <Button onClick={criarTarefa} mr={2} leftIcon={<AddIcon />} variant='solid' borderRadius={0} colorScheme="green" >
+                        Adicionar Tarefa
                     </Button>
-                    <Button leftIcon={<CheckIcon />} variant='solid' colorScheme="green" >
-                        Finalizar todas selecionadas
-                    </Button>
-
-                    <Button leftIcon={<SpinnerIcon />} variant='solid' colorScheme="red" >
-                        Reativar todas selecionadas
-                    </Button>
-                    <Button variant='solid' leftIcon={<CheckCircleIcon />} bg="blue.400" _hover={{ bg: 'blue.500' }} color='white' >
-                        Marcar todas
-                    </Button>
-                </ButtonGroup>
+                    <FormControl display="flex" justifyContent="center" alignItems="center">
+                        <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Titulo</FormLabel>
+                        <Input type="text" p={0} pl={2} py={2} borderRadius="0px" id="tituloCricao" name="tituloCricao" placeholder='Digite o titulo da tarefa aqui...'/>
+                    </FormControl>
+                </ Box>
+                <FormControl mb={3}>
+                    <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Descrição</FormLabel>
+                    <Textarea borderRadius={0}  placeholder='Digite a descrição da tarefa aqui...' id="descCricao" name="descCricao"/>
+                </FormControl>
+               
                 {
                     tarefas.map((tarefa: Tarefa) => {
                         return (
--- projeto/src/paginas/Login.tsx
+++ projeto/src/paginas/Login.tsx
@@ -1,28 +1,126 @@
-import { Box, Button, FormControl, FormLabel, Input, Heading, Text } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { useNavigate, } from "react-router-dom";
+import {
+  Box, Button, FormControl, FormLabel, Input, Heading, IconButton,
+  Tabs, TabList, Tab, TabPanels, TabPanel,
+  useToast
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon  } from '@chakra-ui/icons'
 
 const Login = () => {
+  const navegador = useNavigate();
+
+  const [exibirSenhaEntrar, setExibirSenhaEntrar] = useState<boolean>(false);
+  const exibeSenhaEntrar = () => setExibirSenhaEntrar(!exibirSenhaEntrar);
+
+  const [exibirSenhaCriar, setExibirSenhaCriar] = useState<boolean>(false);
+  const exibeSenhaCriar = () => setExibirSenhaCriar(!exibirSenhaCriar);
+
+  const [exibirSenhaCriarConfirma, setExibirSenhaCriarConfirma] = useState<boolean>(false);
+  const exibeSenhaCriarConfirma = () => setExibirSenhaCriarConfirma(!exibirSenhaCriarConfirma);
+
+  const [senhaCriada, setSenhaCriada] = useState<string>("");
+  const [senhaCriadaConfirma, setSenhaCriadaConfirma] = useState<string>("");
+
+  const aoMudarSenha = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    setSenhaCriada(evento.target.value);
+  }
+  const aoMudarSenhaConfimacao = (evento: React.ChangeEvent<HTMLInputElement>) => {
+    setSenhaCriadaConfirma(evento.target.value);
+  }
+
+  const aviso = useToast();
+
+  const entrarEnviar = () => {
+    navegador('lista/');
+  }
+  const criarEnviar = () => {
+    let erro = false;
+
+    if (senhaCriada.length < 1) {
+      aviso({
+        position: 'top',
+        title: "Tamanho da senha Invalido.",
+        status: "warning",
+        isClosable: true,
+      });
+      erro = true;
+    };
+
+    if (senhaCriada != senhaCriadaConfirma) {
+      aviso({
+        position: 'top',
+        title: "A senha esta diferente da Confirmação da senha, corrija isso",
+        status: "warning",
+        isClosable: true,
+      })
+      erro = true;
+    };
+    
+    if (!erro) navegador('lista/');
+  }
+
   return (
     <Box minH="100vh" display="flex" alignItems="center" justifyContent="center" bg="gray.100">
-      <Box bg="white" p={6} rounded="md" shadow="md" width="100%" maxW="md">
-        <Heading as="h1" size="lg" textAlign="center" mb={10}>
-          Entrar
-        </Heading>
-        <FormControl mt={5} display="flex" justifyContent="center" alignItems="center" id="name">
-          <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Nome</FormLabel>
-          <Input name="titulo" type="text" placeholder="Digite seu usuario aqui..." p={0} pl={2} py={2}  borderRadius="0px" />
-        </FormControl>
+      <Box bg="white" p={6} rounded="md" shadow="md" width="40%" >
+        <Tabs size='md'>
+          <TabList >
+            <Tab>
+              <Heading as="h1" size="lg" textAlign="center" >
+                Entrar
+              </Heading>
+            </Tab>
+            <Tab>
+              <Heading as="h1" size="lg" textAlign="center" >
+                Cadastrar
+              </Heading>
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <FormControl mt={2} display="flex" justifyContent="center" alignItems="center">
+                <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Nome</FormLabel>
+                <Input type="text" placeholder="Digite seu usuario aqui..." p={0} pl={2} py={2} borderRadius="0px" id="usuarioEntrar" name="usuarioEntrar"/>
+              </FormControl>
 
-        <FormControl mt={5} display="flex" justifyContent="center" alignItems="center" id="name">
-          <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Senha</FormLabel>
-          <Input name="desc" type="text" placeholder="Digite sua senha aqui..." p={0} pl={2} py={2}  borderRadius="0px" />
-        </FormControl>
-        
-        <Box display="flex" justifyContent="center" alignItems="center">
-          <Button  mt={6} bg="blue.400" color="white" _hover={{ bg: "blue.600"}} type="submit">
-            Enviar
-          </Button>
-        </Box>
-        <Text color="gray" textAlign="center" mt={4} fontSize="85%">Cheguei atrasado então não sei do que se trata esse site, mas, se quiser entrar, preencha, e, não vá para lugar nenhum</Text>
+              <FormControl mt={5} display="flex" justifyContent="center" alignItems="center">
+                <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Senha</FormLabel>
+                <Input type={!exibirSenhaEntrar ? 'password' : 'text'} placeholder="Digite sua senha aqui..." p={0} pl={2} py={2} borderRadius="0px" id="senhaEntrar" name="senhaEntrar"/>
+                <IconButton onClick={exibeSenhaEntrar} rounded="0" bg="blue.400" _hover={{ bg: 'blue.500' }} color='white' aria-label='Search database' icon={exibirSenhaEntrar ? <ViewOffIcon /> : <ViewIcon />} />
+              </FormControl>
+
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <Button onClick={entrarEnviar} mt={6} bg="blue.400" color="white" _hover={{ bg: "blue.600" }} type="submit">
+                  Entrar
+                </Button>
+              </Box>
+            </TabPanel>
+            <TabPanel>
+              <FormControl mt={2} display="flex" justifyContent="center" alignItems="center">
+                <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Nome</FormLabel>
+                <Input type="text" placeholder="Digite seu usuario aqui..." p={0} pl={2} py={2} borderRadius="0px" id="usuarioCriar" name='usuarioCriar'/>
+              </FormControl>
+
+              <FormControl  mt={5} display="flex" justifyContent="center" alignItems="center">
+                <FormLabel bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Senha</FormLabel>
+                <Input value={senhaCriada} onChange={aoMudarSenha} type={!exibirSenhaCriar ? 'password' : 'text'} placeholder="Digite sua senha aqui..." p={0} pl={2} py={2} borderRadius="0px" id="senhaCriar" name="senhaCriar"/>
+                <IconButton onClick={exibeSenhaCriar}  rounded="0" bg="blue.400" _hover={{ bg: 'blue.500' }} color='white' aria-label='Search database' icon={exibirSenhaCriar ? <ViewOffIcon /> : <ViewIcon />} />
+              </FormControl>
+
+              <FormControl  mt={5} display="flex" justifyContent="center" alignItems="center" >
+                <FormLabel width="200px" bg="blue.400" color="white" borderRadius={0} m={0} p={2}>Confirme Senha</FormLabel>
+                <Input value={senhaCriadaConfirma} onChange={aoMudarSenhaConfimacao} type={!exibirSenhaCriarConfirma ? 'password' : 'text'} placeholder="Digite sua senha aqui..." p={0} pl={2} py={2} borderRadius="0px" id="senhaConfirmar" name="senhaConfirmar"/>
+                <IconButton onClick={exibeSenhaCriarConfirma} rounded="0" bg="blue.400" _hover={{ bg: 'blue.500' }} color='white' aria-label='Search database' icon={exibirSenhaCriarConfirma ? <ViewOffIcon /> : <ViewIcon />} />
+              </FormControl>
+
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <Button onClick={criarEnviar} mt={6} bg="blue.400" color="white" _hover={{ bg: "blue.600" }} type="submit">
+                  Criar
+                </Button>
+              </Box>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Box>
     </Box>
   );
